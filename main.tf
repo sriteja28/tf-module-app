22fd20910234fac5a483eb083e7e5d9c1be64cb4
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-05 13:49:44 +0530

    Updating Terraform files for the project

--- main.tf
+++ main.tf
@@ -39,6 +39,23 @@ resource "aws_lb_target_group" "main" {
 }
 
 
+resource "aws_lb_listener_rule" "main" {
+  listener_arn = var.listener_arn
+  priority     = var.lb_rule_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.component}-${var.env}.sritejadevops.com"]
+    }
+  }
+}
+
+
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
 
@@ -89,7 +106,7 @@ resource "aws_autoscaling_group" "main" {
 ## DNS record R53
 resource "aws_route53_record" "dns" {
   zone_id = "Z070672135BYB8H2ZSHPN"
-  name    = "${var.component}-dev"
+  name    = "${var.component}-${var.env}"
   type    = "CNAME"
   ttl     = 30
   records = [var.lb_dns_name]
