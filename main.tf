7b7c703ebff9e3f6db179640a3134f33b04c1eb0
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-10-05 11:16:32 +0530

    Updating Terraform files for the project

--- main.tf
+++ main.tf
@@ -31,6 +31,14 @@ resource "aws_security_group" "main" {
 }
 
 
+resource "aws_lb_target_group" "main" {
+  name     = "${var.component}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+
 resource "aws_launch_template" "main" {
   name = "${var.component}-${var.env}"
 
@@ -70,6 +78,7 @@ resource "aws_autoscaling_group" "main" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnets
+  target_group_arns = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.main.id
